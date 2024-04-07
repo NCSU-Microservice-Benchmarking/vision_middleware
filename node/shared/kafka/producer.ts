53383083756eafbb4fcc7515a23c4dee5b29e5c7
Kagwe Muchane <kamuchan@ncsu.edu>
2024-04-06 20:51:51 -0400

    more debugging.

--- node/shared/kafka/producer.ts
+++ node/shared/kafka/producer.ts
@@ -45,7 +45,7 @@ class Producer implements Microservice.Producer {
   }
 
   public async send(message: request, topic?: string): Promise<void> {
-    console.log(typeof this.producer);
+    console.log(this.producer);
     console.log(typeof this.producer.produce);
     return new Promise<void>(async (resolve, reject) => {
       this.producer.produce(
